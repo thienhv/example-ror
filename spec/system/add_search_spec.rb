2595a5d0fe730a3e63680a60bb7402ef6434cb58
Ho Viet Thien <thien.hoviet@gmail.com>
2018-04-04 20:02:18 +0700

    Fix checkbox

--- spec/system/add_search_spec.rb
+++ spec/system/add_search_spec.rb
@@ -4,37 +4,33 @@ require 'rails_helper'
 
 RSpec.describe 'Search function', type: :system do
   before do
-    ruby_programming_language = create :programming_language, name: 'Ruby'
-    create :programming_language, name: 'Javascript'
-    create :programming_language, name: 'Php'
-    create :language, code: 'vn'
-
-    @ruby_devloper = create :developer,
-                            email: 'phan.nguyen@example.com',
-                            programming_languages: [ruby_programming_language]
-
-    @ruby_devloper_english = create :developer,
-                                    email: 'quang.pham@example.com',
-                                    programming_languages: [ruby_programming_language],
-                                    languages: [create(:language, code: 'en')]
+    @language = create(:language)
+    @programming_language = create :programming_language
+    @developer = create :developer, programming_languages: [@programming_language]
+    @developer_have_language = create :developer,
+                                      programming_languages: [@programming_language],
+                                      languages: [@language]
   end
 
   it 'shows matched records when a programming language is specified' do
     visit('/')
 
-    select 'Ruby', from: 'search_developer_form[programming_language_id]'
+    select @programming_language.name, from: 'search_developer_form[programming_language_id]'
     click_on 'Search'
 
-    expect(page).to have_content(@ruby_devloper.email)
+    expect(page).to have_content(@developer.email)
+    expect(page).to have_select('search_developer_form[programming_language_id]', selected: @programming_language.name)
   end
 
   it 'shows matched records when a programming language & language are specified' do
     visit('/')
 
-    select 'Ruby', from: 'search_developer_form[programming_language_id]'
-    select 'en', from: 'search_developer_form[language_id]'
+    select @programming_language.name, from: 'search_developer_form[programming_language_id]'
+    select @language.code, from: 'search_developer_form[language_id]'
 
     click_on 'Search'
-    expect(page).to have_content(@ruby_devloper_english.email)
+    expect(page).to have_content(@developer_have_language.email)
+    expect(page).to have_select('search_developer_form[programming_language_id]', selected: @programming_language.name)
+    expect(page).to have_select('search_developer_form[language_id]', selected: @language.code)
   end
 end
